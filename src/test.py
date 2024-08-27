d3727d767133b0b1f35499b79f1e75ac67095982
qweradsf2 <devtokyo@yandex.com>
2024-08-27 12:39:52 +0300

    end

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-
-# with open(r'C:\dev\it_mentor\fast api\doc_image.jpg', 'rb') as image_file:
-#     encoded_string = base64.b64encode(image_file.read())
-#     print(encoded_string)
-
-
-print('hello python')
